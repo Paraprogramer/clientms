b70b26674b71ff95b5a2994707fdf97b955893c0
Bobby Fiando <bobfiando@gmail.com>
2018-02-28 08:08:39 +0700

    login controller & model

---
+++ application/controllers/Login.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+    $this->isLogin();
+		$this->load->model('MLogin');
+	}
+
+  public function index(){
+      $this->load->view('login');
+  }
+
+	public function check()
+	{
+    $result = $this->MLogin->validate();
+
+    if(! $result){
+      $msg = '<font color=red>Invalid username and/or password.</font><br />';
+      $this->index($msg);
+    }else{
+        redirect('client/daftar');
+    }
+	}
+
+  private function isLogin(){
+    if(! $this->session->userdata('validated')){
+        redirect('login');
+    }
+  }
+
+  public function do_logout(){
+      $this->session->sess_destroy();
+      redirect('login');
+  }
+}
---
+++ application/models/MLogin.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MClients extends CI_Model {
+
+	public function validate($table)
+	{
+    $username = $this->security->xss_clean($this->input->post('username'));
+    $password = $this->security->xss_clean($this->input->post('password'));
+
+    $this->db->where('username', $username);
+    $this->db->where('password', $password);
+
+    $query = $this->db->get('users');
+
+    if($query->num_rows == 1)
+    {
+        $row = $query->row();
+        $data = array(
+                'username' => $row->username,
+                'validated' => true
+                );
+        $this->session->set_userdata($data);
+        return true;
+    }
+    return false;
+	}
+}
